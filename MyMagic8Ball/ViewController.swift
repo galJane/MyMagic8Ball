a9108c8e5e4f7ba915c1aa51d1154e5a4546f9c2
Jane <JaneBethPerez@gmail.com>
2020-02-24 15:39:23 -0600

    Initial Commit

---
+++ MyMagic8Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMagic8Ball
+//
+//  Created by Socstudent on 2/24/20.
+//  Copyright © 2020 Jane Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
