ae5ed7212b4166b89ceeacfa201dda180b119a88
Jane <JaneBethPerez@gmail.com>
2020-02-25 14:57:17 -0600

    now

---
+++ MyMagic8Ball/openingViewController.swift
@@ -0,0 +1,45 @@
+//
+//  openingViewController.swift
+//  MyMagic8Ball
+//
+//  Created by Socstudent on 2/25/20.
+//  Copyright © 2020 Jane Perez. All rights reserved.
+//
+
+import UIKit
+
+class openingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
